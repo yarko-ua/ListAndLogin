c5cb9d742db4986d752c014e5fdeda6df70e8db9
Yaroslav <ssilvias13@gmail.com>
2020-02-12 16:34:10 +0200

    first commit

---
+++ build/precache-manifest.1d5536e32a94b03ae9ce5b99303c1133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6f7931f44d1e865264c8a80661c0a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb8ed6f0f36b2ae0272",
+    "url": "/static/css/main.f1226403.chunk.css"
+  },
+  {
+    "revision": "80c4a81e0db7f6b70678",
+    "url": "/static/js/2.4cc0c6ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4cc0c6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb8ed6f0f36b2ae0272",
+    "url": "/static/js/main.6f48eef7.chunk.js"
+  },
+  {
+    "revision": "18d9252af1fba34cf0ba",
+    "url": "/static/js/runtime-main.83662137.js"
+  }
+]);
